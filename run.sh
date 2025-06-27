994bd7e19e4a313447b4e4e330105dd34b54d674
Your Name <keller@fzi.de>
2025-06-27 18:58:12 +0200

    reduced run.sh with only 1 gpu and without srun

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 dataset=$1
 
+mkdir -p log/${dataset}
 
-for repeat_id in 0 1 2 3 4 5 6 7 8 9
+for repeat_id in 0 # 1 2 3 4 5 6 7 8 9
 do
-	for shot in  1 5 10
-	do
-		srun python train_net.py --num-gpus 8  --config-file ./configs/${dataset}/${shot}_shot.yaml 2>&1 | tee log/${dataset}/${shot}_shot.log
-		
-	done
+  for shot in  10 # 1 5 10
+  do
+    python train_net.py --num-gpus 1  --config-file ./configs/${dataset}/${shot}_shot.yaml 2>&1 | tee log/${dataset}/${shot}_shot.log
+  done
 done
